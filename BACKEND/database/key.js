bc3db5f0ee8c7d99826400ffae542d62984bf809
miguel <miguel.ramirez7@unmsm.edu.pe>
2020-11-02 01:24:39 -0500

    cambios

--- BACKEND/database/key.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database: {
-      secretD: 'miSecreto',
-      secretU: 'usuariosecret',
-      URI: 'mongodb+srv://aicos:SXzVuOIPKhXFgAK9@cluster0.vzuc9.mongodb.net/sicram2020?retryWrites=true&w=majority'
-    }
-  };
-  
