2768ade33410f31570ce38c70f10fbc8a6866184
unknown <richard.qqueccano@unmsm.edu.pe>
2020-11-01 20:50:54 -0500

    package

---
+++ BACKEND/database/key.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+      secretD: 'miSecreto',
+      secretU: 'usuariosecret',
+      URI: 'mongodb+srv://rpopi:holamundo2020@cluster0.scxnr.mongodb.net/prubaSicram?retryWrites=true&w=majority'
+    }
+  };
+  
