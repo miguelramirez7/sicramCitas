7673e832efc4c8c09a0ae9c1e6d91dd2789627c1
miguel <miguel.ramirez7@unmsm.edu.pe>
2020-11-02 01:36:43 -0500

    funcionalidad:  + endpoints   Login LogOut

--- BACKEND/routes/api/indexct.js
+++ BACKEND/routes/api/indexct.js
@@ -3,8 +3,12 @@ require('../../config/userpassport')(passport);
 var express = require('express');
 var router = express.Router();
 var doctorController = require("../../controller/doctorController");
+
 //crera un nuevo usuario REGISTANDOTE
 router.post('/signupdoctor', doctorController.SignupDoctor);
 //LOGEARTE una vez ya tengas tu CUENTA REGISTRADA
 router.post('/signindoctor', doctorController.SigninDoctor);
+//salir de la cuenta del doctor
+router.get('/signoutdoctor', passport.authenticate('doctor', { session: false}), doctorController.SignoutDoctor);
+
 module.exports = router;
