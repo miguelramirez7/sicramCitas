6f34027f906e56c302e5d9fc07a5283799d7ad54
unknown <richard.qqueccano@unmsm.edu.pe>
2020-11-02 01:01:46 -0500

    Arreglando doctor

--- BACKEND/config/userpassport.js
+++ BACKEND/config/userpassport.js
@@ -4,7 +4,7 @@ var JwtStrategy = require('passport-jwt').Strategy,
 // cargando y usando el modelo usuario
 var User = require('../models/user');
 var config = require('../database/key');//para obtener el secreto
-
+var Doctor  = require('../models/doctor');
 module.exports = function(passport) {
   var opts = {};
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
@@ -26,5 +26,23 @@ module.exports = function(passport) {
       });
   }));
 
+
+  //estrategia para encontrar al Doctor a partir del token y nos retornarar el user que sera de doctor
+  passport.use('doctor',new JwtStrategy(opts, function(jwt_payload, done) {
+    Doctor.findOne({username: jwt_payload.username},  function(err, user ){
+            if (err) {
+                
+                return done(err, false);
+            }
+            if (user) {
+              console.log(user.username + " " + user.id);
+              done(null, user);
+              
+             } else {
+                return done(null, false);
+             }
+            
+      });
+  }));
  
 };
--- BACKEND/routes/api/indexct.js
+++ BACKEND/routes/api/indexct.js
@@ -5,6 +5,6 @@ var router = express.Router();
 var doctorController = require("../../controller/doctorController");
 //crera un nuevo usuario REGISTANDOTE
 router.post('/signupdoctor', doctorController.SignupDoctor);
-//LOGEARTE una vez ya tengas tu CUENTA REGISTRADA
-router.post('/signindoctor', doctorController.SigninDoctor);
+
+
 module.exports = router;
