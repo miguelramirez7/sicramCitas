21caccd100408f5b92ec729451e3b2092f05169c
miguel <miguel.ramirez7@unmsm.edu.pe>
2020-11-09 02:44:13 -0500

    cambio

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -16,7 +16,7 @@ require('./config/userpassport');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
