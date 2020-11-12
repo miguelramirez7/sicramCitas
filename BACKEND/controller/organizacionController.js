e6b5272906a78dffe0015554efad6d5ceb23b3d9
unknown <richard.qqueccano@unmsm.edu.pe>
2020-11-12 13:52:58 -0500

    Agregando rutas y horarios

--- BACKEND/controller/organizacionController.js
+++ BACKEND/controller/organizacionController.js
@@ -6,7 +6,7 @@ var Organizacion = require("../models/organizacion");
 var Especialidad = require("../models/especialidad");
 exports.SignupOrganizacion = async function (req, res) {
     try {
-      if (!req.body.username || !req.body.password || !req.body.email) {
+      if (!req.body.password || !req.body.email) {
         res.json({
           success: false,
           msg: "Por favor, ponga nombre de usuario y contraseña",
@@ -21,7 +21,7 @@ exports.SignupOrganizacion = async function (req, res) {
               res.status(401).json({ msg: "email ya esta siendo usado" });
             } else {
               var newOrg = new Organizacion({
-                username: req.body.username,
+
                 password: req.body.password,
                 email: req.body.email,
                 nameOrg: req.body.nameOrg,
@@ -37,7 +37,7 @@ exports.SignupOrganizacion = async function (req, res) {
               await newOrg.save(function (error, newOrga) {
                 if (error) {
                   
-                  console.log("ususario en uso");
+                  console.log("usuario en uso");
                   return res.json({ success: false, msg: "Username ya existe." });
                 }
                 res.status(200).json({
