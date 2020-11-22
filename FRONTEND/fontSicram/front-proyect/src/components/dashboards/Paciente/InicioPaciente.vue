26bf14579bbc53084a88e74194bccd5a5e6a3532
jorge <jorge.nu.jm@gmail.com>
2020-11-21 22:30:42 -0500

    inicio paciente

--- FRONTEND/fontSicram/front-proyect/src/components/dashboards/Paciente/InicioPaciente.vue
+++ FRONTEND/fontSicram/front-proyect/src/components/dashboards/Paciente/InicioPaciente.vue
@@ -1,14 +1,27 @@
 <template>
-   <div class="">
-        <h1>InicioPaciente</h1>
-         <h1>InicioPaciente</h1>
-          <h1>InicioPaciente</h1>
-           <h1>InicioPaciente</h1>
-            <h1>InicioPaciente</h1>
-             <h1>InicioPaciente</h1>
-              <h1>InicioPaciente</h1>
-             
-   </div>
+   <div class="d-flex justify-center flex-column align-center" > 
+        <h1 class="text-center">BIENVENIDO(A) A SICRAM</h1>
+        <h3>Consultorio de citas médicas</h3>
+        <v-container>
+            <v-row>
+                <v-col cols="12" md="6" class="imagenBienvenida">
+                    <img src="../../../assets/bienvenida.jpg" alt="">
+                </v-col>
+                <v-col cols="12" md="6" class="contenidoBienvenida">
+                    <div class="">
+                        <p><span class="guinBienvenida">-</span> <span class="font-weight-bold">¿Quiénes somos?</span> </p>
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi vel delectus minima quidem quos voluptatum at iste ut consequuntur in obcaecati, laborum sed consectetur et!</span>
+                    </div>
+                    
+                    <div class="">
+                        <p>  <span class="guinBienvenida">-</span> <span class="font-weight-bold">¿Qué hacemos?</span> </p>
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi vel delectus minima quidem quos voluptatum at iste ut consequuntur in obcaecati, laborum sed consectetur et!</span>
+                    </div>
+                  
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
