980c861071fe01901ef6bbd13fdc66de0b9c0bc5
jorge <jorge.nu.jm@gmail.com>
2020-11-21 21:15:25 -0500

    vista perfil organizacion

--- FRONTEND/fontSicram/front-proyect/src/components/dashboards/Organizacion/TuPerfilOrg.vue
+++ FRONTEND/fontSicram/front-proyect/src/components/dashboards/Organizacion/TuPerfilOrg.vue
@@ -1,58 +1,128 @@
- <template>
-     <div class="">
-         <v-container>
-             <v-row>
-                 <v-col cols="12" md="4" class="d-flex justify-center align-center">
-                     <v-avatar size="180" >
-                         <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
-                     </v-avatar>
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="5">
+          <v-card content-class="carta-perfil" class="mt-5">
 
-                 </v-col>
-                 <v-col cols="12" md="5">
-                     <v-card elevation="4" class="mt-5">
-                         <v-card-title class="titulo" >Información Personal</v-card-title>
-                         <v-card-text>
-                             <v-row>
-                                 <v-col cols="6" md="6">
-                                        <p class="font-weight-bold"> NOMBRES Y APELLIDOS:</p>
-                                        <p class="font-weight-bold"> DNI:</p>
-                                        <p class="font-weight-bold"> USERNAME:</p>
-                                        <p class="font-weight-bold"> EDAD:</p>
-                                        <p class="font-weight-bold"> DISCAPACIDAD:</p>
-                                        <p class="font-weight-bold"> CELULAR:</p>
-                                        <p class="font-weight-bold"> DIRECCIÓN:</p>
-                                        
-                                 </v-col>
-                                 <v-col cols="6" md="6">
-                                        <p > NOMBRES Y APELLIDOS:</p>
-                                        <p > DNI:</p>
-                                        <p> USERNAME:</p>
-                                        <p > EDAD:</p>
-                                        <p > DISCAPACIDAD:</p>
-                                        <p> CELULAR:</p>
-                                        <p > DIRECCIÓN:</p>
-                                 </v-col>
-                             </v-row>
-                         </v-card-text>
-                     </v-card>
-                 </v-col>
-             </v-row>
-         </v-container>
-     </div>
+            <div class="imagen-card">
+              <img src="../../../assets/organizacion.svg" alt="John" />
+            </div>
 
- </template>
 
- <script>
-     export default {
+            <v-card-title class="titulo2">NOMBRE COMPLETO</v-card-title>
+            <v-card-subtitle class="text-center">
+              Doctor
+            </v-card-subtitle>
+          </v-card>
 
-     }
- </script>
+        </v-col>
+        <v-col cols="12" md="6">
+          
+            
+              <div class="mt-5">
+                <v-row justify="center">
+                  <v-expansion-panels accordion>
+                    <v-expansion-panel value="true">
+                      <v-expansion-panel-header class="titulo">Información </v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        <v-simple-table>
+                          <template v-slot:default>
+                            <tbody>
+                              <tr v-for="item in infoPersonal" :key="item.name">
+                                <td>
+                                  <v-icon small class="mr-2" v-text="item.icon"></v-icon>{{ item.name }}
+                                </td>
+                                <td>{{ item.calories }}</td>
+                              </tr>
+                            </tbody>
+                          </template>
+                        </v-simple-table>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+
+                   
+                  </v-expansion-panels>
+                </v-row>
+              </div>
+            
+
+          
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  import {
+    mapActions,
+    mapGetters
+  } from 'vuex';
+  export default {
+    name: 'TuPerfilDoc',
+    computed: {
+      ...mapGetters(['getDoctorPerfil'])
+    },
+    data() {
+      return {
+        infoPersonal: [{
+            name: 'NOMBRES Y APELLIDOS',
+            calories: '-----',
+            icon: 'mdi-rename-box'
+          },
+          {
+            name: 'DNI',
+            calories: 237,
+            icon: 'mdi-credit-card'
+          },
+          {
+            name: 'EDAD',
+            calories: 262,
+            icon: 'mdi-clock'
+          },
+          {
+            name: 'TELEFONO',
+            calories: 305,
+            icon: 'mdi-card-account-phone'
+          },
+          {
+            name: 'CELULAR',
+            calories: 356,
+            icon: 'mdi-cellphone'
+          },
+          {
+            name: 'CORREO',
+            calories: 375,
+            icon: 'mdi-email'
+          },
+          {
+            name: 'GENERO',
+            calories: 392,
+            icon: 'mdi-gender-male-female-variant'
+          },
+        ],
+        infoProfesional: [{
+            name: 'ESPECIALIDAD',
+            calories: 'Pediatra',
+            icon: 'mdi-school'
+          },
+          {
+            name: 'CMP',
+            calories: 237,
+            icon: 'mdi-tag'
+          },
+          
+          
+        ],
+      }
+    }
+
+
+  };
+</script>
 
  <style  scoped>
-     .v-card__title {
-         justify-content: center;
-    font-size: 1.7rem;
-    font-weight: 600;
-    color: rgb(8, 150, 114);
-}
+ 
+@import "../../../assets/css/orga.css";
  </style>
