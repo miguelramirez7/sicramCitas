bf29afcb9a323f5916cd0fa698eafc657b027b50
Aicos <richard.sw268@gmail.com>
2020-12-13 19:20:01 -0500

    back-configuracion de cron- desplegado en heroku

--- BACKEND/database/evento.js
+++ BACKEND/database/evento.js
@@ -10,28 +10,30 @@ var m = n.getMonth() + 1;
 var d = n.getDate();
 const fechaActual = y + "-" + m + "-" + d;
 // Este evento borra todos los horarios pasados, se verifica siempre al finalizar el día
-cron.schedule("0 0 * * *", async () => {
-  await Horario.find(
-    { fecha: { $lt: fechaActual }, ocupado: false },
-    async (err, horarios) => {
-      if (err) {
-        console.log("ERROR" + err);
-      }
-      if (horarios) {
-         await Doctor.find({ id: horarios.doctor }, (err, doctores) => {
+
+  cron.schedule("5 0 0 * * *", async () => {
+    await Horario.find(
+      { fecha: { $lt: fechaActual }, ocupado: false },
+      async (err, horarios) => {
+        if (err) {
+          console.log("ERROR" + err);
+        }
+        if (horarios) {
+          await Doctor.find({ id: horarios.doctor }, (err, doctores) => {
             doctores.forEach((doctor) => {
               horarios.forEach((horario) => {
                 var encontrado = doctor.horario.indexOf(horario.id);
                 if (encontrado > -1) {
-                  doctor.horario.splice(encontrado,1);
+                  doctor.horario.splice(encontrado, 1);
                   horario.deleteOne();
                 }
               });
               doctor.save();
             });
-            
           });
+        }
       }
-    }
-  );
-});
+    );
+  });
+
+
